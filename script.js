57a81c529ab0b4759d3d74c40c37379acd251ed1
zbob889 <zroberts889@gmail.com>
2022-06-22 15:48:08 +0200

    Got it to change the object values of what is clicked

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const gameBoard = (() => {
 
-    const boardCells = (mark) => {
-        return { mark };
+    const boardCells = (name, mark) => {
+        return { name, mark };
     };
 
-    const topRight = boardCells('empty');
-    const topMiddle = boardCells('empty');
-    const topLeft = boardCells('empty');
-    const centerRight = boardCells('empty');
-    const centerMiddle = boardCells('empty');
-    const centerLeft = boardCells('empty');
-    const bottomRight = boardCells('empty');
-    const bottomMiddle = boardCells('empty');
-    const bottomLeft = boardCells('empty');
+    const currentBoard = [
+        boardCells('topRight', 'empty'),
+        boardCells('topMiddle', 'empty'),
+        boardCells('topLeft', 'empty'),
+        boardCells('centerRight', 'empty'),
+        boardCells('centerMiddle', 'empty'),
+        boardCells('centerLeft', 'empty'),
+        boardCells('bottomRight', 'empty'),
+        boardCells('bottomMiddle', 'empty'),
+        boardCells('bottomLeft', 'empty'),
+    ];
 
-    const currentBoard = [topRight, topMiddle, topLeft, centerRight, centerMiddle, centerLeft, bottomRight, bottomMiddle, bottomLeft];
+    return currentBoard;
 
 })();
 
@@ -35,11 +37,18 @@ const displayController = (() => {
             e.target.className = 'x';
             e.target.innerHTML = '<svg width="150px" height="150px" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 1.5L13.5 13.5M1.5 13.5L13.5 1.5" stroke="black"/></svg>';
             count++;
+
+            let object = gameBoard.find(element => element.name === e.target.getAttribute('id'));
+            object.mark = 'x'; 
+
         } else if ((count % 2) === 0 && e.target.className === 'cell'){
             // mark it o
             e.target.className = 'x';
             e.target.innerHTML = '<svg width="150px" height="150px" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.5 7.5C0.5 11.366 3.63401 14.5 7.5 14.5C11.366 14.5 14.5 11.366 14.5 7.5C14.5 3.63401 11.366 0.5 7.5 0.5C3.63401 0.5 0.5 3.63401 0.5 7.5Z" stroke="black"/></svg>';
             count++;
+
+            let object = gameBoard.find(element => element.name === e.target.getAttribute('id'));
+            object.mark = 'o'; 
         };
     });
 
