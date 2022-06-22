a208e68fca8f2e8123285492d05178fa2d6c69b1
zbob889 <zroberts889@gmail.com>
2022-06-22 15:11:05 +0200

    New game button works

--- script.js
+++ script.js
@@ -26,17 +26,8 @@ const displayController = (() => {
 
     let count = 1;
 
-    const topRightCell = document.getElementById('topRight');
-    const topMiddleCell = document.getElementById('topMiddle');
-    const topLeftCell = document.getElementById('topLeft');
-    const centerRightCell = document.getElementById('centerRight');
-    const centerMiddleCell = document.getElementById('centerMiddle');
-    const centerLeftCell = document.getElementById('centerLeft');
-    const bottomRightCell = document.getElementById('bottomRight');
-    const bottomMiddleCell = document.getElementById('bottomMiddle');
-    const bottomLeftCell = document.getElementById('bottomLeft');
-
     const board = document.getElementById('board');
+    const newGame = document.getElementById('newGame');
 
     board.addEventListener('click', (e) => {
         if((count % 2) != 0 && e.target.className === 'cell'){
@@ -49,6 +40,15 @@ const displayController = (() => {
             e.target.className = 'x';
             e.target.innerHTML = '<svg width="150px" height="150px" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.5 7.5C0.5 11.366 3.63401 14.5 7.5 14.5C11.366 14.5 14.5 11.366 14.5 7.5C14.5 3.63401 11.366 0.5 7.5 0.5C3.63401 0.5 0.5 3.63401 0.5 7.5Z" stroke="black"/></svg>';
             count++;
+        };
+    });
+
+    newGame.addEventListener('click', () => {
+        count = 1;
+        const cells = document.querySelectorAll('.x');
+        for (const x of cells){
+            x.className = 'cell';
+            x.innerHTML = '';
         };
     });
 
