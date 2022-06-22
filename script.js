49189ae2af56f331c29952f8a305633a37af29bb
zbob889 <zroberts889@gmail.com>
2022-06-22 14:41:13 +0200

    Got board clicks working

--- script.js
+++ script.js
@@ -16,9 +16,40 @@ const gameBoard = (() => {
 
     const currentBoard = [topRight, topMiddle, topLeft, centerRight, centerMiddle, centerLeft, bottomRight, bottomMiddle, bottomLeft];
 
-
 })();
 
 const players = (name) => {
     return { name };
 };
+
+const displayController = (() => {
+
+    let count = 1;
+
+    const topRightCell = document.getElementById('topRight');
+    const topMiddleCell = document.getElementById('topMiddle');
+    const topLeftCell = document.getElementById('topLeft');
+    const centerRightCell = document.getElementById('centerRight');
+    const centerMiddleCell = document.getElementById('centerMiddle');
+    const centerLeftCell = document.getElementById('centerLeft');
+    const bottomRightCell = document.getElementById('bottomRight');
+    const bottomMiddleCell = document.getElementById('bottomMiddle');
+    const bottomLeftCell = document.getElementById('bottomLeft');
+
+    const board = document.getElementById('board');
+
+    board.addEventListener('click', (e) => {
+        if((count % 2) != 0 && e.target.className === 'cell'){
+            // mark it x
+            e.target.className = 'x';
+            e.target.innerHTML = '<svg width="150px" height="150px" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 1.5L13.5 13.5M1.5 13.5L13.5 1.5" stroke="black"/></svg>';
+            count++;
+        } else if ((count % 2) === 0 && e.target.className === 'cell'){
+            // mark it o
+            e.target.className = 'x';
+            e.target.innerHTML = '<svg width="150px" height="150px" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.5 7.5C0.5 11.366 3.63401 14.5 7.5 14.5C11.366 14.5 14.5 11.366 14.5 7.5C14.5 3.63401 11.366 0.5 7.5 0.5C3.63401 0.5 0.5 3.63401 0.5 7.5Z" stroke="black"/></svg>';
+            count++;
+        };
+    });
+
+})();
