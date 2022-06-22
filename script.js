fa26ea00eead6c5fe7ddf7b65aadf30216ae8c83
zbob889 <zroberts889@gmail.com>
2022-06-22 15:54:37 +0200

    Modified new game button to change all objects back to empty

--- script.js
+++ script.js
@@ -37,16 +37,13 @@ const displayController = (() => {
             e.target.className = 'x';
             e.target.innerHTML = '<svg width="150px" height="150px" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 1.5L13.5 13.5M1.5 13.5L13.5 1.5" stroke="black"/></svg>';
             count++;
-
             let object = gameBoard.find(element => element.name === e.target.getAttribute('id'));
             object.mark = 'x'; 
-
         } else if ((count % 2) === 0 && e.target.className === 'cell'){
             // mark it o
             e.target.className = 'x';
             e.target.innerHTML = '<svg width="150px" height="150px" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.5 7.5C0.5 11.366 3.63401 14.5 7.5 14.5C11.366 14.5 14.5 11.366 14.5 7.5C14.5 3.63401 11.366 0.5 7.5 0.5C3.63401 0.5 0.5 3.63401 0.5 7.5Z" stroke="black"/></svg>';
             count++;
-
             let object = gameBoard.find(element => element.name === e.target.getAttribute('id'));
             object.mark = 'o'; 
         };
@@ -58,6 +55,9 @@ const displayController = (() => {
         for (const x of cells){
             x.className = 'cell';
             x.innerHTML = '';
+        };
+        for (const e of gameBoard){
+            e.mark = 'empty';
         };
     });
 
